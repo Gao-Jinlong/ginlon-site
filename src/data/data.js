3c2f493cc93039af85feb94e7f481952b6d63398
gao-jinlong <2675130594@qq.com>
2024-08-07 23:35:29 +0800

    feat:card

--- src/data/data.js
+++ src/data/data.js
@@ -1,11 +1,7 @@
 export const techCardsData = [
   {
-    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-plain.svg',
-    alt: 'HTML5',
-  },
-  {
-    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-plain.svg',
-    alt: 'CSS3',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vuejs/vuejs-original.svg',
+    alt: 'Vuejs',
   },
   {
     url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-plain.svg',
@@ -16,35 +12,51 @@ export const techCardsData = [
     alt: 'TypeScript',
   },
   {
-    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/less/less-plain-wordmark.svg',
-    alt: 'LESS',
-  },
-  {
-    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postcss/postcss-original.svg',
-    alt: 'PostCSS',
-  },
-  {
-    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg',
-    alt: 'TailwindCSS',
-  },
-  {
     url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg',
     alt: 'SASS',
   },
   {
-    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/astro/astro-original.svg',
-    alt: 'Astro',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg',
+    alt: 'Vite',
   },
   {
-    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg',
-    alt: 'React',
+    url: 'https://openlayers.org/theme/img/logo-dark.svg',
+    alt: 'Openlayers',
   },
   {
     url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg',
     alt: 'Git',
   },
   {
-    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg',
-    alt: 'Vite',
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg',
+    alt: 'TailwindCSS',
+  },
+  {
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/webpack/webpack-original.svg',
+    alt: 'Webpack',
+  },
+  {
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nestjs/nestjs-original.svg',
+    alt: 'Nestjs',
+  },
+  {
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/astro/astro-original.svg',
+    alt: 'Astro',
+  },
+  {
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/babel/babel-original.svg',
+    alt: 'Babel',
+  },
+  {
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/pnpm/pnpm-original.svg',
+    alt: 'Pnpm',
+  },
+  {
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg',
+    alt: 'React',
+  },
+  {
+    url: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/rust/rust-original.svg',
+    alt: 'Rust',
   },
 ];
