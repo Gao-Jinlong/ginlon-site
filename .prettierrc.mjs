3f2d7ee75fb375303e56147a45695753f1be28a0
ginlon <ginlon5241@gmail.com>
2024-12-30 23:03:11 +0800

    chore: config

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,16 +1,22 @@
 /** @type {import("prettier").Options} */
 const prettierConfig = {
-  plugins: ["prettier-plugin-astro"],
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
   overrides: [
     {
-      files: "*.astro",
+      files: '*.{md,mdx}',
       options: {
-        parser: "astro",
+        parser: 'markdown',
+      },
+    },
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
       },
     },
   ],
   singleQuote: true,
-  arrowParens: "avoid",
+  arrowParens: 'avoid',
 };
 
 export default prettierConfig;
